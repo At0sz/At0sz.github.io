128d843d3d571e0af3f7803dd048829553e8ee18
unknown <atoszfilms@gmail.com>
2022-06-10 22:38:33 +0200

    calc v1.1

--- js/calc.js
+++ js/calc.js
@@ -1,13 +1,4 @@
-/*function doMath(){
-    let number1 = document.getElementById('num1').value;
-    let number2 = document.getElementById('num2').value;
-    console.log(number1);
-    console.log(number2);
-    let finnish =parseInt(number1) + parseInt(number2);
-    console.log(finnish);
-    let p =document.getElementById('summa');
-    p.innerHTML = finnish;
-}*/
+
 
 
 
@@ -15,32 +6,47 @@ function doPlus(){
     let number1 = document.getElementById('num1').value;
     let number2 = document.getElementById('num2').value;
     let finnish =parseInt(number1) + parseInt(number2);
-    let p =document.getElementById('summa');
-    p.innerHTML = finnish;
+    if(isNaN(finnish)){
+        document.getElementById('summa').innerHTML = "";    
+    }else{
+    document.getElementById('summa').innerHTML = finnish;
+    };
 }
 
 function doMinus(){
     let number1 = document.getElementById('num1').value;
     let number2 = document.getElementById('num2').value;
     let finnish =parseInt(number1) - parseInt(number2);
-    let p =document.getElementById('summa');
-    p.innerHTML = finnish;
+     if(isNaN(finnish)){
+        document.getElementById('summa').innerHTML = "";    
+    }else{
+    document.getElementById('summa').innerHTML = finnish;
+    };
+    
 }
 
 function doSzor(){
     let number1 = document.getElementById('num1').value;
     let number2 = document.getElementById('num2').value;
     let finnish =parseInt(number1) * parseInt(number2);
-    let p =document.getElementById('summa');
-    p.innerHTML = finnish;
+     if(isNaN(finnish)){
+        document.getElementById('summa').innerHTML = "";    
+    }else{
+    document.getElementById('summa').innerHTML = finnish;
+    };
+    
 }
 
 function doOszt(){
     let number1 = document.getElementById('num1').value;
     let number2 = document.getElementById('num2').value;
     let finnish =parseInt(number1) / parseInt(number2);
-    let p =document.getElementById('summa');
-    p.innerHTML = finnish;
+     if(isNaN(finnish)){
+        document.getElementById('summa').innerHTML = "";    
+    }else{
+    document.getElementById('summa').innerHTML = finnish;
+    };
+    
 }
 
 function doClear(){
